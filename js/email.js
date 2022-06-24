1c7a391f64681cbb8613159196604f390865ff7d
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-06-24 16:00:00 +0200

    first commit

---
+++ js/email.js
@@ -0,0 +1,28 @@
+/*
+Mail
+Crea (inventa) una lista di inidirizzi mail autorizzati.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
+*/
+
+// creo una lista di email
+//chiedo all' utente un email ( prompt)
+//controllo che il nome scelto sia nella lista 
+//se l' email è presente stampo  il permesso in console
+//se l' email non è presente stampo il divieto in console 
+
+//creo una lista email
+const emails = ['pinco.pallino@io.com', 'solitoignoto@io.com', 'umbrella@io.com']
+console.table(emails);
+//chiedo all' utente un email (prompt)
+const quest = prompt('inserisci un email', 'solitoignoto@io.com')
+
+
+for (let i = 0; i < emails.length; i++){
+    console.log(i)
+ if(quest !== emails.length[0] || quest !==emails.length[1] || quest !==emails.length[2]){
+     console.log('non puoi accedere')
+ }else if ( quest === emails.length[0]  || quest === emails.length[1]|| quest === emails.length[2])
+  console.log('puoi accedere')
+}
